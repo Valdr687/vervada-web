9ec3fe7361229b5b13fa789896482d5a848363b8
Thomas C <106614142+Valdr687@users.noreply.github.com>
2022-07-24 11:05:51 +0200

    Update script.js
    Optimised changetheme(), this function now works with only an unique css file.

--- js/script.js
+++ js/script.js
@@ -2,27 +2,20 @@
 var theme ;
     
 //Fonctions
-function changeCSS(cssFile, cssLinkIndex) {
-   var oldlink = document.getElementsByTagName("link").item(cssLinkIndex);
-   var newlink = document.createElement("link");
-   newlink.setAttribute("rel", "stylesheet");
-   newlink.setAttribute("type", "text/css");
-   newlink.setAttribute("href", cssFile);
-   document.getElementsByTagName("head").item(cssLinkIndex).replaceChild(newlink, oldlink);
-}
-
 function changeTheme() {
   if ( theme == "light" ) {
-    changeCSS('./css/style_accueil_dark.css', 0) ;
+    var element = document.body;
+    element.classList.toggle("dark");
     image = document.getElementById('moon_sun');
     image.src = "./images/icones/dark_light/sun.png";
     theme="dark" ;
   }
   else { 
-    changeCSS('./css/style_accueil.css', 0);
-    theme="light";
+    var element = document.body;
+    element.classList.toggle("dark");
     image = document.getElementById('moon_sun');
     image.src = "./images/icones/dark_light/moon.png";
+    theme="light";
   }	
   return theme
 }
@@ -31,9 +24,7 @@ function changeTheme() {
 const userPrefersDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
 const userPrefersLight = window.matchMedia && window.matchMedia('(prefers-color-scheme: light)').matches;
 if(userPrefersDark){
-  changeCSS('./css/style_accueil_dark.css', 0) ;
-  image = document.getElementById('moon_sun');
-  image.src = "./images/icones/dark_light/sun.png";
+  
   theme="dark" ;
 }
 else {
